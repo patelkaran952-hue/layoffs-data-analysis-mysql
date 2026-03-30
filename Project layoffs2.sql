24c1d218096ebed1dd74dd627f05f4267f5973e4
patelkaran952-hue <patelkaran952@gmail.com>
2026-03-30 19:29:18 +0530

    Add files via upload
    Removed duplicate records using ROW_NUMBER()

---
+++ Project layoffs2.sql
@@ -0,0 +1,92 @@
+select *
+from layoffsgpt;
+
+create table layoffsgpt2
+like layoffsgpt;
+
+insert layoffsgpt2
+select *
+from layoffsgpt;
+
+select *
+from layoffsgpt2;
+
+
+-- Remove Duplicates
+
+SELECT *,
+ROW_NUMBER() OVER(
+    PARTITION BY company, location, total_laid_off, percentage_laid_off, industry, stage, funds_raised, country
+) AS row_num
+FROM layoffsgpt2;
+
+with duplicate_cte as
+(
+select *,
+ROW_NUMBER() OVER(
+    PARTITION BY company, location, total_laid_off, percentage_laid_off, industry, stage, funds_raised, country
+) AS row_num
+FROM layoffsgpt2
+)
+select * 
+from duplicate_cte
+where row_num > 1;
+
+select *
+from layoffsgpt2
+where company = '2U';
+
+CREATE TABLE `layoffsgpt3` (
+  `company` text,
+  `location` text,
+  `total_laid_off` text,
+  `date` text,
+  `percentage_laid_off` text,
+  `industry` text,
+  `source` text,
+  `stage` text,
+  `funds_raised` int DEFAULT NULL,
+  `country` text,
+  `date_added` text,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+insert into layoffsgpt3
+select *,
+ROW_NUMBER() OVER(
+    PARTITION BY company, location, total_laid_off, percentage_laid_off, industry, stage, funds_raised, country
+) AS row_num
+FROM layoffsgpt2;
+
+delete
+from layoffsgpt3
+where row_num > 1;
+
+select *
+from layoffsgpt3;
+
+
+-- Standardize Data
+
+select company, trim(company)
+from layoffsgpt3;
+
+UPDATE layoffsgpt2
+SET 
+    company = TRIM(company),
+    location = TRIM(location),
+    industry = TRIM(industry),
+    country = TRIM(country),
+    stage = TRIM(stage);
+
+
+
+
+
+
+
+
+
+
+
+
